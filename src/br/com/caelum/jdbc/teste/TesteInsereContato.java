4b8b7677311a2c448906330ff5f375b16157ce62
Gabriel Morais Barbosa <ga-morais@hotmail.com>
2015-11-19 21:21:50 -0200

    Inserindo e Mostrando Contatos
    Persistência de um contato no banco e mostrando todos os contatos
    cadastrados no banco de dados!

--- src/br/com/caelum/jdbc/teste/TesteInsereContato.java
+++ src/br/com/caelum/jdbc/teste/TesteInsereContato.java
@@ -1,6 +1,10 @@
 package br.com.caelum.jdbc.teste;
 
 import java.sql.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
 import br.com.caelum.jdbc.DAO.ContatoDAO;
@@ -8,14 +12,25 @@ import br.com.caelum.jdbc.modelo.Contato;
 
 public class TesteInsereContato {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
 		ContatoDAO contatoDAO = new ContatoDAO();
 		
-		Contato contato = contatoDAO.pesquisaContatoById(2);
+		//Inserindo contato novo
+		Contato novo = new Contato();
+		novo.setNome("Zezim da silva");
+		novo.setEndereco("Rua da pipoca");
+		novo.setEmail("zezim@hotmail.com");
+		DateFormat smf = new SimpleDateFormat("yyyy/MM/dd");
+		java.sql.Date date = new java.sql.Date(smf.parse("2000/10/05").getTime());
+		novo.setDataNascimento(date);
 		
-		MostraContao(contato);
+		contatoDAO.adicionaContato(novo);
+		//Mostrando contato de numero 2
+		//Contato contato = contatoDAO.pesquisaContatoById(2);
+		//MostraContao(contato);
 		
+		//Mostrando todos os contatos
 		MostraTodosOsContatos(contatoDAO.pesquisaAllContatos());
 		
 	}
